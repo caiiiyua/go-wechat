65376a04b4417816a0590692ee6420bcf0808e44
Yuanqing Cai <ckjacket@gmail.com>
2015-02-25 02:18:37 +0800

    first commit

---
+++ wechat.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func wechatHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	signature := r.Form.Get("signature")
+	timestamp := r.Form.Get("timestamp")
+	nonce := r.Form.Get("nonce")
+	echostr := r.Form.Get("echostr")
+	fmt.Fprintf(w, "hello wechat! request[%s, %s, %s, %s]", signature, timestamp, nonce,
+		echostr)
+}
+
+func main() {
+	http.HandleFunc("/wechat", wechatHandler)
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal("Start wechat server failed")
+	} else {
+		log.Println("Start wechat server and listening on 80 ...")
+	}
+}

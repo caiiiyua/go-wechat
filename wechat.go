cf1d5a0cbc3f154e8958b55c2fc20932acf6925d
Yuanqing Cai <ckjacket@gmail.com>
2015-02-28 15:53:16 +0800

    add msg handler

--- wechat.go
+++ wechat.go
@@ -11,6 +11,8 @@ import (
 )
 
 const token string = "nehe"
+const appid string = "wxe039839fbc011f6d"
+const appsecret string = "929194a70c73eed9f8bec14528a2b8c2"
 
 func makeSignature(t string, ts string, nonce string) string {
 	s1 := []string{t, ts, nonce}
@@ -29,21 +31,45 @@ func validate(w http.ResponseWriter, r *http.Request) bool {
 		log.Fatalln("Reuqest is not a valid request from Wechat!")
 		return false
 	}
-	fmt.Fprintf(w, echostr)
+	if len(echostr) > 0 {
+		fmt.Fprintf(w, echostr)
+	}
 	log.Println("Validate request from Wechat successfully!")
 	return true
 }
 
+func respBuilder(category string) string {
+	return "test"
+}
+
+// postMsgHandler is for "POST" request of http
+func postMsgHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// messageHandler is handling all of messages from wechat
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "POST":
+		fmt.Println("POST request", r)
+		postMsgHandler(w, r)
+	default:
+		fmt.Println("default message handler")
+	}
+
+}
+
 func wechatHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	echostr := r.Form.Get("echostr")
-	if len(echostr) > 0 {
-		if !validate(w, r) {
-			log.Fatalln("validate failed")
-		}
+
+	if !validate(w, r) {
+		log.Fatalln("validate failed")
 	}
 	fmt.Fprintf(w, "default handler")
 	fmt.Println("default handler")
+
+	// message handler
+	messageHandler(w, r)
 }
 
 func main() {
